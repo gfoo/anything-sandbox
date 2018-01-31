a14660fa3546a788217a0ec0438e70bdff24d134
gfoo <gilles.faucherand@gmail.com>
2018-01-31 10:41:27 +0100

    fix tests

--- src/app/views/edition/edition.component.spec.ts
+++ src/app/views/edition/edition.component.spec.ts
@@ -1,17 +1,21 @@
+import { AppMaterialModule } from './../../app-material.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditionComponent } from './edition.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('EditionComponent', () => {
   let component: EditionComponent;
   let fixture: ComponentFixture<EditionComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ EditionComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [AppMaterialModule, BrowserAnimationsModule],
+        declarations: [EditionComponent]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditionComponent);
--- src/app/views/things/things.component.spec.ts
+++ src/app/views/things/things.component.spec.ts
@@ -1,17 +1,30 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './../../app-material.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ThingsComponent } from './things.component';
+import { BackendService } from '../../services/backend.service';
 
 describe('ThingsComponent', () => {
   let component: ThingsComponent;
   let fixture: ComponentFixture<ThingsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ThingsComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [AppMaterialModule, BrowserAnimationsModule],
+        declarations: [ThingsComponent],
+        providers: [
+          {
+            provide: BackendService,
+            useClass: class BackendServiceMockup {
+              // add fake methods if required by tests
+            }
+          }
+        ]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ThingsComponent);
