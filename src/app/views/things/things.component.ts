d4a3e99248dcb4ab9dcddd19ef1f43e90f156b26
gfoo <gilles.faucherand@gmail.com>
2018-02-08 11:35:42 +0100

    LL fake: actu accordion + toolbar color...

--- src/app/views/things/things.component.ts
+++ src/app/views/things/things.component.ts
@@ -2,7 +2,7 @@ import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { ThingModel } from '../../models/thing.model';
 import { BackendService, ThingBag } from './../../services/backend.service';
-import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 import { Observable } from 'rxjs/Observable';
 import { of as observableOf } from 'rxjs/observable/of';
@@ -10,22 +10,27 @@ import { catchError } from 'rxjs/operators/catchError';
 import { map } from 'rxjs/operators/map';
 import { startWith } from 'rxjs/operators/startWith';
 import { switchMap } from 'rxjs/operators/switchMap';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'any-things',
   templateUrl: './things.component.html',
   styleUrls: ['./things.component.css']
 })
-export class ThingsComponent implements OnInit {
+export class ThingsComponent implements OnInit, AfterViewInit {
   displayedColumns = ['label', 'id', 'rights'];
   dataSource = new MatTableDataSource<ThingModel>();
   nbRows = 10;
   resultsLength = 0;
   isLoadingResults = true;
-
+  @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private backendService: BackendService) {}
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit() {
     this.paginator.page
@@ -50,6 +55,14 @@ export class ThingsComponent implements OnInit {
           return observableOf([]);
         })
       )
-      .subscribe((data: ThingBag) => (this.dataSource.data = data.things));
+      .subscribe((data: ThingBag) => {
+        this.dataSource.data = data.things;
+      });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
 }
